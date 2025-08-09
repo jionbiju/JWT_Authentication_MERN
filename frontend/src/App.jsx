1d2fbd37003902c73db3886bd6d8f19ccb5edff0
jionbiju <jionbiju@gmail.com>
2025-08-09 21:45:17 +0530

    folder structure created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-center text-red-600 text-4xl'>
+        Does tailwind work
+    </div>
+  )
+}
+
+export default App
